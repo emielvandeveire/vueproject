f5f23e527794ffa81bba00d03d1586d16f27c3fa
Emiel Van de Veire <emielvdv@MacBook-Pro-van-Emiel.local>
2021-11-20 11:56:18 +0100

    register, login and great functionality'

--- src/views/ForgotPassword.vue
+++ src/views/ForgotPassword.vue
@@ -1,13 +1,73 @@
 <template>
-  <div></div>
+  <div class="reset-password">
+    <Modal
+      v-if="modalActive"
+      :modalMessage="modalMessage"
+      v-on:close-modal="closeModal"
+    />
+    <Loading v-if="loading" />
+    <div class="form-wrap">
+      <form class="reset">
+        <p class="login-register">
+          Back to
+          <router-link class="router-link" :to="{ name: 'Login' }"
+            >Login</router-link
+          >
+        </p>
+        <h2>Reset Password</h2>
+        <p>Forgot your passowrd? Enter your email to reset it</p>
+        <div class="inputs">
+          <div class="input">
+            <input type="text" placeholder="Email" v-model="email" />
+            <email class="icon" />
+          </div>
+        </div>
+        <button>Reset</button>
+        <div class="angle"></div>
+      </form>
+      <div class="background"></div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
+import email from "../assets/Icons/envelope-regular.svg";
+import Modal from "../components/Modal";
+import Loading from "../components/Loading";
 
-}
+export default {
+  name: "ForgotPassword",
+  data() {
+    return {
+      email: null,
+      modalActive: false,
+      modalMessage: "",
+      loading: null,
+    };
+  },
+  components: {
+    email,
+    Modal,
+    Loading,
+  },
+  methods: {
+    closeModal() {
+      this.modalActive = !this.modalActive;
+      this.email = "";
+    },
+  },
+};
 </script>
 
-<style>
-
+<style scoped>
+.reset-password {
+  position: relative;
+}
+.reset-password .form-wrap .reset h2 {
+  margin-bottom: 8px;
+}
+.reset-password .form-wrap .reset p {
+  text-align: center;
+  margin-bottom: 32px;
+}
 </style>
